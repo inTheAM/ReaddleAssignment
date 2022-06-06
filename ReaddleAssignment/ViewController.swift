8cd27e28928efcdab669ecd46f21bc4059f0d74e
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-06 08:01:16 +0300

    Initial Commit

---
+++ ReaddleAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReaddleAssignment
+//
+//  Created by Ahmed Mgua on 06/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
