ab516c8a574c465f4381fbc98ef7ff67e23597f1
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-06 08:34:47 +0300

    Add tableview

--- ReaddleAssignment/ViewController.swift
+++ ReaddleAssignment/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: view.bounds)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func loadView() {
         view = UIView()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Readdle"
+    
+        setUpTableView()
     }
     
     override func viewDidLoad() {
@@ -22,5 +30,13 @@ class ViewController: UIViewController {
     }
 
 
+    private func setUpTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
 }
 
