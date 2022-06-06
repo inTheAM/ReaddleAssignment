077b19fcfe8de57a5fddbec7de3eea697dcbb887
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-06 08:23:45 +0300

    Set up initial viewcontroller

--- ReaddleAssignment/ViewController.swift
+++ ReaddleAssignment/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Readdle"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
