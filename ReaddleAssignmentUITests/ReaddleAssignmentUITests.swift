9d07ffb88bfaddd323892b2fe06b74a068c1cc2c
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-08 13:33:26 +0300

    Add testUIElementsExistOnLaunch

--- ReaddleAssignmentUITests/ReaddleAssignmentUITests.swift
+++ ReaddleAssignmentUITests/ReaddleAssignmentUITests.swift
@@ -9,33 +9,35 @@ import XCTest
 
 class ReaddleAssignmentUITests: XCTestCase {
 
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launchArguments = ["MockData"]
+        
+        app.launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app.terminate()
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testUIElementsExistOnLaunch() throws {
+        let header = app.navigationBars["Readdle"]
+        let layoutToggle = app.navigationBars.buttons["switch-layout-button"]
+        let collectionView = app.collectionViews["files-collection-view"]
+        let mockFileIcon = app.cells["file-icon"]
+        let iconImage = app.images["file-image"]
+        let filename = app.staticTexts["file.pdf"]
+        
+        
+        XCTAssert(header.waitForExistence(timeout: 5))
+        XCTAssert(layoutToggle.waitForExistence(timeout: 5))
+        XCTAssert(collectionView.waitForExistence(timeout: 5))
+        XCTAssert(mockFileIcon.waitForExistence(timeout: 5))
+        XCTAssert(iconImage.waitForExistence(timeout: 5))
+        XCTAssert(filename.waitForExistence(timeout: 5))
     }
 }
