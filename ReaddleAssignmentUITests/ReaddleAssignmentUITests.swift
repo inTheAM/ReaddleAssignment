ea98126fd10ab446c0e56006a308e28d025b8868
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-08 13:53:26 +0300

    Add testSwitchingLayoutChangesButtonIcon

--- ReaddleAssignmentUITests/ReaddleAssignmentUITests.swift
+++ ReaddleAssignmentUITests/ReaddleAssignmentUITests.swift
@@ -26,7 +26,7 @@ class ReaddleAssignmentUITests: XCTestCase {
 
     func testUIElementsExistOnLaunch() throws {
         let header = app.navigationBars["Readdle"]
-        let layoutToggle = app.navigationBars.buttons["switch-layout-button"]
+        let layoutToggle = app.navigationBars.buttons["toggle-list-button"]
         let collectionView = app.collectionViews["files-collection-view"]
         let mockFileIcon = app.cells["file-icon"]
         let iconImage = app.images["file-image"]
@@ -39,5 +39,19 @@ class ReaddleAssignmentUITests: XCTestCase {
         XCTAssert(mockFileIcon.waitForExistence(timeout: 5))
         XCTAssert(iconImage.waitForExistence(timeout: 5))
         XCTAssert(filename.waitForExistence(timeout: 5))
+    }
+    
+    func testSwitchingLayoutChangesButtonIcon() throws {
+        let listToggle = app.navigationBars.buttons["toggle-list-button"]
+        _ = listToggle.waitForExistence(timeout: 5)
+        
+        listToggle.tap()
+        XCTAssertFalse(listToggle.exists)
+        
+        let gridToggle = app.navigationBars.buttons["toggle-grid-button"]
+        XCTAssert(gridToggle.waitForExistence(timeout: 5))
+        gridToggle.tap()
+        XCTAssertFalse(gridToggle.exists)
+        XCTAssert(listToggle.waitForExistence(timeout: 5))
     }
 }
