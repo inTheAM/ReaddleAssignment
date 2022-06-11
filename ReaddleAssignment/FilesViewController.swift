5e58d2d4b6c5b0bec021514dd1c7262f3a576882
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-11 08:28:01 +0300

    Set FilesViewController to inherit from FileTreeViewController

--- ReaddleAssignment/FilesViewController.swift
+++ ReaddleAssignment/FilesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FilesViewController.swift
 //  ReaddleAssignment
 //
 //  Created by Ahmed Mgua on 06/06/2022.
@@ -7,30 +7,12 @@
 
 import UIKit
 
-class FilesViewController: UIViewController {
+final class FilesViewController: FileTreeViewController {
+    
     private let navigationTitle = "Files"
     
-    /// The data source and delegate for the collection view
+    /// The data source for the collection view
     private let viewModel = ViewModel()
-    
-    private lazy var collectionView: UICollectionView = {
-        let layout = FileExplorerLayout()
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(FileIcon.self, forCellWithReuseIdentifier: "icon")
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.accessibilityIdentifier = "files-collection-view"
-        return collectionView
-    }()
-    
-    private lazy var switchLayoutButton: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(systemName: "list.bullet"), for: .normal)
-        button.addTarget(self, action: #selector(switchLayout), for: .touchUpInside)
-        button.frame = .init(x: 0, y: 0, width: 40, height: 40)
-        button.accessibilityLabel = "Switch layout from grid to list"
-        button.accessibilityIdentifier = "toggle-list-button"
-        return button
-    }()
     
     override func loadView() {
         view = UIView()
@@ -38,7 +20,7 @@ class FilesViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = navigationTitle
         
-        setUpCollectionView()
+        setUpCollectionView(dataSource: viewModel)
         setUpNavigationButtons()
     }
     
@@ -48,50 +30,13 @@ class FilesViewController: UIViewController {
             collectionView?.reloadData()
         }
     }
-    
-    private func updateAccessibilityLabels() {
-        switchLayoutButton.accessibilityLabel = "Switch layout from " + (FileExplorerLayout.layoutType == .grid ? "grid to list" : "list to grid")
-        switchLayoutButton.accessibilityIdentifier = "toggle-" + (FileExplorerLayout.layoutType == .grid ? "list" : "grid") + "-button"
-    }
-    
-    private func setUpCollectionView() {
-        view.addSubview(collectionView)
-        collectionView.dataSource = viewModel
-        collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
-        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-    }
-    
-    private func setUpNavigationButtons() {
-        let button = UIBarButtonItem(customView: switchLayoutButton)
-        navigationItem.rightBarButtonItem = button
-    }
-    
-    /// Toggles the collection view layout between grid and list views
-    @objc private func switchLayout() {
-        let layout = FileExplorerLayout()
-        switch FileExplorerLayout.layoutType {
-        case .grid:
-            FileExplorerLayout.layoutType = .list
-            switchLayoutButton.setImage(UIImage(systemName: "square.grid.2x2"), for: .normal)
-        default:
-            FileExplorerLayout.layoutType = .grid
-            switchLayoutButton.setImage(UIImage(systemName: "list.bullet"), for: .normal)
-        }
-        reload(collectionView)
-        
-        updateAccessibilityLabels()
-        self.collectionView.setCollectionViewLayout(layout, animated: true)
-    }
-    
-    func reload(_ collectionView: UICollectionView) {
-        
-        let contentOffset = collectionView.contentOffset
-        collectionView.reloadData()
-        collectionView.layoutIfNeeded()
-        collectionView.setContentOffset(contentOffset, animated: false)
-        
+}
+
+
+extension FilesViewController {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let file = viewModel.files[indexPath.item]
+        showDetailViewController(file)
     }
 }
 
