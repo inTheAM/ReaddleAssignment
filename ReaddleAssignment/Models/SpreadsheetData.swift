02fc460f90182ca6d8a58356e0f59bc68f20c608
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-15 12:28:31 +0300

    Add manual Coddle conformance to SpreadsheetData and add SpreadsheetUpdateData

--- ReaddleAssignment/Models/SpreadsheetData.swift
+++ ReaddleAssignment/Models/SpreadsheetData.swift
@@ -7,7 +7,39 @@
 
 import Foundation
 
-struct SpreadsheetData: Decodable {
+struct SpreadsheetData: FileTreeRepresentable {
     let range: String
-    let values: [[String]]
+    let values: [FileItem]
 }
+
+extension SpreadsheetData: Codable {
+    enum CodingKeys: String, CodingKey {
+        case range = "range",
+        values = "values"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(range, forKey: .range)
+        let stringValues = Self.encodeFiles(values)
+        try container.encode(stringValues, forKey: .values)
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container    =    try    decoder.container(keyedBy: CodingKeys.self)
+        range    =    try container.decode(String.self,    forKey:    .range)
+        
+        let stringValues = try container.decode([[String]].self, forKey: .values)
+        let files = Self.decodeFiles(stringValues)
+        let fileTree = Self.organize(files)
+        
+        values = fileTree
+    }
+}
+
+#if DEBUG
+extension SpreadsheetData {
+    static let empty = SpreadsheetData(range: "", values: [])
+}
+#endif
+
---
+++ ReaddleAssignment/Models/SpreadsheetUpdateData.swift
@@ -0,0 +1,12 @@
+//
+//  SpreadsheetUpdateData.swift
+//  ReaddleAssignment
+//
+//  Created by Ahmed Mgua on 15/06/2022.
+//
+
+import Foundation
+
+struct SpreadsheetUpdateData: Decodable {
+    let updatedData: SpreadsheetData
+}
