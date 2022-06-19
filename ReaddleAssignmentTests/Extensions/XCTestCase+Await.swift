1bd7e0b032cbfec9ba955212cdda18bc2e8f513e
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-19 08:34:30 +0300

    Refactor await extension

---
+++ ReaddleAssignmentTests/Extensions/XCTestCase+Await.swift
@@ -0,0 +1,52 @@
+//
+//  XCTestCase+Await.swift
+//  ReaddleAssignmentTests
+//
+//  Created by Ahmed Mgua on 19/06/2022.
+//
+
+import Foundation
+
+extension XCTestCase {
+    /// Awaits the result of a publisher and returns the output or throws an error.
+    /// - Parameters:
+    ///   - publisher: The publisher to subscribe to for results.
+    ///   - timeout: The time to wait for a result, after which
+    ///              an error is thrown if the publisher did not output.
+    ///   - file: The file in which the method was called when an error is thrown.
+    ///   - line: The line in which the method was called when an error is thrown.
+    /// - Returns: The output of the publisher.
+    func awaitResult<T: Publisher>(
+        from publisher: T,
+        timeout: TimeInterval = 10,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws -> T.Output {
+        var result: Result<T.Output, Error>?
+        let expectation = self.expectation(description: "Awaiting publisher")
+        
+        let cancellable = publisher
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    result = .failure(error)
+                case .finished:
+                    break
+                }
+                expectation.fulfill()
+            },
+                  receiveValue: { value in
+                result = .success(value)
+            }
+            )
+        waitForExpectations(timeout: timeout)
+        cancellable.cancel()
+        
+        let unwrappedResult = try XCTUnwrap(result,
+                                            "Awaited publisher did not produce any output",
+                                            file: file,
+                                            line: line)
+        
+        return try unwrappedResult.get()
+    }
+}
