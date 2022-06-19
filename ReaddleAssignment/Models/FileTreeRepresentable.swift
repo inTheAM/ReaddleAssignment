dfb56bad83dd129366ef2ecbc43d0ecac7fb0f6a
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-19 08:23:20 +0300

    Fix file tree organization

--- ReaddleAssignment/Models/FileTreeRepresentable.swift
+++ ReaddleAssignment/Models/FileTreeRepresentable.swift
@@ -7,18 +7,42 @@
 
 import Foundation
 
+/// A protocol for types that can be converted to and from`FileItem`s.
 protocol FileTreeRepresentable {
     
+    /// Decodes an aray of values from the GoogleSheets API into an array of `FileItem`
+    /// - Parameter array: The values to convert to FileItem
+    /// - Returns: The array of file items converted.
+    static func decodeFiles(_ array: [Array<String>]) -> [FileItem]
+    
+    /// Encodes `FileItem` into the array of values that can be sent to the Google Sheets API.
+    /// - Parameter files: The files to encode.
+    /// - Returns: The array of values representing the item's properties.
+    static func encodeFiles(_ files: [FileItem]) -> [Array<String>]
+    
+    /// Organizes a flat array of file items into a file tree.
+    /// - Parameter files: The files to organize
+    /// - Returns: An array containing top level elements with children sorted into parents.
+    static func organize(_ files: [FileItem]) -> [FileItem]
 }
 
+// MARK: - Default implementations
 extension FileTreeRepresentable {
+    
     static func decodeFiles(_ array: [Array<String>]) -> [FileItem] {
-        return array.map { fields -> FileItem in
+        var index = 0
+        return array.compactMap { fields -> FileItem? in
+            if fields.first == nil || fields.first!.isEmpty {
+                print("RETURNING NIL FOR INDEX \(index)")
+                index += 1
+                return nil
+            }
+            index += 1
             let id: String = fields[0]
-            let parentID: String? = fields[1].isEmpty ? nil : fields[1]
+            let parentID: String = fields[1]
             let fileType = FileItem.FileType(rawValue: fields[2])!
             let filename = fields[3]
-            return FileItem(id: UUID(uuidString: id)!, parentID: UUID(uuidString: parentID ?? ""), name: filename, fileType: fileType)
+            return FileItem(id: UUID(uuidString: id)!, parentID: UUID(uuidString: parentID), range: "A\(index):D\(index)", name: filename, fileType: fileType, children: fileType == .directory ? [] : nil)
         }
     }
     
@@ -32,47 +56,38 @@ extension FileTreeRepresentable {
         }
     }
     
-    private static func assignToParent(_ file: FileItem, to parentID: UUID, in parents: inout [FileItem]) {
-        guard let index = parents.firstIndex(where: { $0.id == parentID })  else {
-            fatalError("Parent does not exist!")
-        }
-        if parents[index].children == nil {
-            parents[index].children = [file]
-        } else {
-            parents[index].children?.append(file)
-        }
-        
-    }
-    
     static func organize(_ files: [FileItem]) -> [FileItem] {
         
-        var fileItems = [FileItem]()
+        // Files with parent folders
+        let childFiles = files.filter { $0.parentID != nil && $0.fileType == .file }
+        
+        // All folders
         let allFolders = files.filter { $0.fileType == .directory }
-        let allFiles = files.filter { $0.fileType == .file }
         
-        fileItems.append(contentsOf: allFolders)
-        
-        // Assign each file to parent folder if existing
-        for file in allFiles {
-            if let parentID = file.parentID {
-                assignToParent(file, to: parentID, in: &fileItems)
-            } else {
-                fileItems.append(file)
+        // Assigning child files to parent folders.
+        for file in childFiles {
+            if let parentFolderID = file.parentID,
+               let parentFolderIndex = allFolders.firstIndex(where: { $0.id == parentFolderID }) {
+                allFolders[parentFolderIndex].children?.append(file)
             }
         }
         
-        // Assign folders to parent folders if existing.
-        for (index, fileItem) in fileItems.enumerated() {
-            if fileItem.fileType == .directory,
-               let parentID = fileItem.parentID {
-                assignToParent(fileItem, to: parentID, in: &fileItems)
-                fileItems.remove(at: index)
+        // Files without parent folders
+        let topLevelFiles = files.filter { $0.parentID == nil && $0.fileType == .file }
+        
+        // Folders without parent folders
+        let topLevelFolders = allFolders.filter { $0.parentID == nil && $0.fileType == .directory }
+        var childFolders = allFolders.filter { $0.parentID != nil }
+        
+        // Create file tree by assigning folders to parent folders.
+        while !childFolders.isEmpty {
+            for parent in topLevelFolders {
+                parent.assignChildren(from: &childFolders)
             }
         }
         
-        // Sort files by name and return
-        fileItems.sort { $0.name < $1.name }
-        
-        return fileItems
+        let organizedFiles = (topLevelFolders + topLevelFiles).sorted(by: {$0.range < $1.range})
+        dump(organizedFiles)
+        return organizedFiles
     }
 }
