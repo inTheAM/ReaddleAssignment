a9ec00c6cee6bc5ffd9c695aa2c6ffcdc8562b3a
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-19 12:07:35 +0300

    Add view model tests adding and deleting items

--- ReaddleAssignmentTests/Tests/ViewModelTests.swift
+++ ReaddleAssignmentTests/Tests/ViewModelTests.swift
@@ -31,7 +31,34 @@ class ViewModelTests: XCTestCase {
         XCTAssertEqual(file.children?[0].id, FileItem.samples[0].id)
     }
 
+    func testAddingItem() throws {
+        let addedItemPublisher = viewModel.file
+            .dropFirst()
+            .first()
+        
+        viewModel.addNewFileItem("file3.pdf", type: .file)
+        
+        let file = try awaitResult(from: addedItemPublisher)
+        XCTAssertEqual(file.children?[0].name, "file3.pdf")
+    }
 
+    func testDeletingItem() throws {
+        let addedItemPublisher = viewModel.file
+            .dropFirst()
+            .first()
+        
+        viewModel.addNewFileItem("file3.pdf", type: .file)
+        
+        _ = try awaitResult(from: addedItemPublisher)
+        
+        let deletedItemPublisher = viewModel.file
+            .dropFirst()
+            .first()
+        
+        viewModel.delete(at: 0)
+        let deleted = try awaitResult(from: deletedItemPublisher)
+        XCTAssertNil(deleted.children?.first)
+    }
 }
 
 
