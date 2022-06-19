b09ca512b76fb0a8f9b4d37d0349dc9f45164c14
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-19 08:22:57 +0300

    Add comments

--- ReaddleAssignment/Models/SpreadsheetData.swift
+++ ReaddleAssignment/Models/SpreadsheetData.swift
@@ -7,17 +7,24 @@
 
 import Foundation
 
+/// The data received when fetching a spreadsheet from the Google Sheets API.
 struct SpreadsheetData: FileTreeRepresentable {
+    
+    /// The range of cells covered in the spreadsheet
     let range: String
+    
+    /// The values in the spreadsheet
     let values: [FileItem]
 }
 
+// MARK: - Codable conformance
 extension SpreadsheetData: Codable {
     enum CodingKeys: String, CodingKey {
         case range = "range",
         values = "values"
     }
     
+    /// Encodes the spreadsheet data to send over the network
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(range, forKey: .range)
@@ -25,6 +32,7 @@ extension SpreadsheetData: Codable {
         try container.encode(stringValues, forKey: .values)
     }
     
+    /// Decodes spreadsheet data received from the network
     init(from decoder: Decoder) throws {
         let container    =    try    decoder.container(keyedBy: CodingKeys.self)
         range    =    try container.decode(String.self,    forKey:    .range)
@@ -34,6 +42,7 @@ extension SpreadsheetData: Codable {
         let fileTree = Self.organize(files)
         
         values = fileTree
+        
     }
 }
 
