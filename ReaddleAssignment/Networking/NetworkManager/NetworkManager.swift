beafa2eb46134a43d056d9c69e14cdf0aeafeb68
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-19 22:10:00 +0300

    Fix debugging printing

--- ReaddleAssignment/Networking/NetworkManager/NetworkManager.swift
+++ ReaddleAssignment/Networking/NetworkManager/NetworkManager.swift
@@ -72,10 +72,11 @@ extension NetworkManager: NetworkManagerProtocol {
                 }
 #if DEBUG
                 .map { output in
-                    let object = try? JSONSerialization.jsonObject(with: output.data)
-                    let data = try? JSONSerialization.data(withJSONObject: object!, options: [.prettyPrinted])
-                    let prettyPrintedString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                    print("\n==========DATA==========\n", prettyPrintedString as Any, "\n\n")
+                    if let object = try? JSONSerialization.jsonObject(with: output.data),
+                       let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+                       let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+                        print("\n==========DATA==========\n", prettyPrintedString as Any, "\n\n")
+                    }
                     return output.data
                 }
 #else
@@ -119,10 +120,11 @@ extension NetworkManager: NetworkManagerProtocol {
                 }
 #if DEBUG
                 .map { output in
-                    let object = try? JSONSerialization.jsonObject(with: output.data)
-                    let data = try? JSONSerialization.data(withJSONObject: object!, options: [.prettyPrinted])
-                    let prettyPrintedString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                    print("\n==========DATA==========\n", prettyPrintedString as Any, "\n\n")
+                    if let object = try? JSONSerialization.jsonObject(with: output.data),
+                       let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+                       let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+                        print("\n==========DATA==========\n", prettyPrintedString as Any, "\n\n")
+                    }
                     return output.data
                 }
 #else
