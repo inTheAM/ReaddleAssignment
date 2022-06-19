685165b490f9268ff90425cacb4986db28b284cd
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-19 08:29:08 +0300

    Integrate google sign in

--- ReaddleAssignment/AppDelegate.swift
+++ ReaddleAssignment/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Ahmed Mgua on 06/06/2022.
 //
 
+import GoogleSignIn
 import UIKit
 
 @main
@@ -30,7 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
+        
+        handled = GIDSignIn.sharedInstance.handle(url)
+        return handled
+    }
 }
 
