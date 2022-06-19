0c99bb8ce87d8c3cc6444b9939f39eec01568b97
Ahmed Mgua <mguaahmed@gmail.com>
2022-06-19 19:13:32 +0300

    Add mock url protocol

---
+++ ReaddleAssignmentTests/Mocks/MockURLProtocol.swift
@@ -0,0 +1,43 @@
+//
+//  MockURLProtocol.swift
+//  ReaddleAssignmentTests
+//
+//  Created by Ahmed Mgua on 19/06/2022.
+//
+
+import Combine
+import Foundation
+@testable import ReaddleAssignment
+import XCTest
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    static var testURLs = [URL?: Data]()
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            XCTFail("Received unexpected request with no handler set")
+            return
+        }
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {
+    }
+}
